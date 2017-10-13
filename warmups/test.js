9f6b2c4d3579965cdb023969b0b3595cf0202017
Nick Riehl <rhontos@gmail.com>
2017-10-13 16:15:50 -0700

    Functional

--- warmups/test.js
+++ warmups/test.js
@@ -3,6 +3,14 @@
 // }
 
 
-for (let i = 0; i < 5; i++) {
-  setTimeout(function() { console.log(i); }, i * 1000);
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(function() { console.log(i); }, i * 1000);
+// }
+
+const change_return = (coins, total) => {
+  return coins.reduce((counts, coin, i) => {
+    counts[coin] = Math.floor(total / coin)
+  }, {})
 }
+// console.log([25, 12, 5] + 1)
+console.log(change_return([25, 10, 5, 1], 127))
